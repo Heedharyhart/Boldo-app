62848da997452c01b21e3d3cbe35c49ec7524da8
Idayat <idayat.abdulkareem@konga.com>
2022-11-18 15:02:53 +0100

    removed the comma

--- src/atom/Button/button.jsx
+++ src/atom/Button/button.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import styles from './button.scss';
 
 
 const Button = ({ onClick, children, className }) => {
   return (
-    <button className={styles.btn, className} type="button" onClick={onClick}>
+    <button className={className} type="button" onClick={onClick}>
       {children}
     </button>
   );
